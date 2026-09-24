68299760764f0f100830857035fd4d6cfce03cc3
lodimarcello <lodimarcello@gmail.com>
2026-09-24 17:45:04 +0200

    update

--- models/prep/prep_weather_hourly.sql
+++ models/prep/prep_weather_hourly.sql
@@ -1,37 +1,28 @@
-WITH updated_hourly AS (
-SELECT
-	*,
-	DATE_PART('day', timestamp) AS date,
-	DATE_PART('hour', timestamp) AS time,
-	DATE_PART('hour', timestamp)::TEXT AS hour,
-	TO_CHAR(timestamp, 'FMmonth') AS month_name,
-	TO_CHAR(timestamp, 'FMDay') AS weekday,
-	DATE_PART('day', timestamp) AS date_day,
-	DATE_PART('month', timestamp) AS date_month,
-	DATE_PART('year', timestamp) AS date_year,
-	DATE_PART('week', timestamp) AS cw
-FROM
-	{{ref('staging_weather_hourly')}}
-),
-add_hourtime AS (
-SELECT *,
-	CASE
-		WHEN DATE_PART('hour', timestamp) = 0
-	THEN 'midnight'
-		WHEN DATE_PART('hour', timestamp) < 6
-	THEN 'night'
-		WHEN DATE_PART('hour', timestamp) < 12
-	THEN 'morning'
-		WHEN DATE_PART('hour', timestamp) = 12
-	THEN 'midday'
-		WHEN DATE_PART('hour', timestamp) < 18
-	THEN 'afternoon'
-		ELSE 'evening'
-		END AS day_part
-	FROM
-	updated_hourly
-)
-SELECT
-	*
-FROM
-	add_hourtime
+ WITH hourly_data AS (
+        SELECT * 
+        FROM {{ref('staging_weather_hourly')}}
+    ),
+    add_features AS (
+        SELECT *
+        , timestamp::DATE AS date -- only date (year-month-day) as DATE data type
+        , timestamp::TIME AS time -- only time (hours:minutes:seconds) as TIME data type
+            , TO_CHAR(timestamp,'HH24:MI') as hour -- time (hours:minutes) as TEXT data type
+            , TO_CHAR(timestamp, 'FMmonth') AS month_name   -- month name as a TEXT
+            , TO_CHAR(timestamp, 'FMday') AS weekday        -- weekday name as TEXT            
+            , DATE_PART('day', timestamp) AS date_day
+    		, DATE_PART('month', timestamp) AS date_month
+    		, DATE_PART('year', timestamp) AS date_year
+    		, DATE_PART('week', timestamp) AS cw
+        FROM hourly_data
+    ),
+    add_more_features AS (
+        SELECT *
+    		,(CASE 
+    			WHEN time BETWEEN '00:00:00' AND '05:59:00' THEN 'night'
+    			WHEN time BETWEEN '06:00:00' AND '18:00:00' THEN 'day'
+    			WHEN time BETWEEN '18:00:00' AND '23:59:00' THEN 'evening'
+    		END) AS day_part
+        FROM add_features
+    )
+    SELECT *
+    FROM add_more_features
