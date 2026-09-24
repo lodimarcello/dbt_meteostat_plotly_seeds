54a86a0f92f45c74429dabb87827b032f840201a
lodimarcello <lodimarcello@gmail.com>
2026-09-24 12:08:45 +0200

    sending hourly data

--- models/staging/staging_weather_hourly.sql
+++ models/staging/staging_weather_hourly.sql
@@ -1,16 +1,16 @@
-WITH daily_raw AS (
+WITH hourly_raw AS (
 					SELECT airport_code
 							,station_id
 							,JSON_ARRAY_ELEMENTS(extracted_data -> 'data') AS json_data
-					FROM {{source('weather_data', 'weather_daily_raw')}} --adding the source for our data		
+					FROM {{source('weather_data', 'weather_hourly_raw')}}		
 ),
-daily_flattened AS (
+hourly_flattened AS (
 					SELECT airport_code
 							,station_id
-							,(json_data ->> 'date')::DATE AS date
-							,(json_data ->> 'tavg')::NUMERIC AS avg_temp_c
-							,(json_data ->> 'tmin')::NUMERIC AS min_temp_c
-							,(json_data ->> 'tmax')::NUMERIC AS max_temp_c
+							,(json_data ->> 'time')::DATE AS date
+							,(json_data ->> 'temp')::NUMERIC AS temperature
+							,(json_data ->> 'dwpt')::NUMERIC AS dew_point_c
+							,(json_data ->> 'rhum')::NUMERIC AS relative_humidity_p
 							,(json_data ->> 'prcp')::NUMERIC AS precipitation_mm
 							,(json_data ->> 'snow')::NUMERIC::INTEGER AS max_snow_mm
 							,(json_data ->> 'wdir')::NUMERIC::INTEGER AS avg_wind_direction
@@ -18,6 +18,7 @@ daily_flattened AS (
 							,(json_data ->> 'wpgt')::NUMERIC AS avg_peakgust
 							,(json_data ->> 'pres')::NUMERIC AS avg_pressure_hpa
 							,(json_data ->> 'tsun')::NUMERIC::INTEGER AS sun_minutes
-						FROM daily_raw
+							,(json_data ->> 'coco')::NUMERIC::INTEGER AS condition_code
+						FROM hourly_raw
 )
-SELECT * FROM daily_flattened --remove semicolon ; as it's not needed for dbt
+SELECT * FROM hourly_flattened
